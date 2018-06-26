969bf688c9d3a189a03b3ef58790769ec61332a4
Callum Rogers <callumrogers@hotmail.co.uk>
2018-06-26 11:54:09 +0100

    Remove the old stuff I was making

--- local-aws-lambda-java8/src/main/java/uk/callumr/local/aws/lambda/config/EventConfig.java
+++
@@ -1,7 +0,0 @@
-package uk.callumr.local.aws.lambda.config;
-
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
-@JsonTypeInfo(use=JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT)
-public interface EventConfig {
-}
