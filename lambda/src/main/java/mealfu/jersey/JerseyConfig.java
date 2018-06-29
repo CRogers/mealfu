fdfb0e0d8dd0a8007f1dc35bc8afd3d07c7296a4
Callum Rogers <callumrogers@hotmail.co.uk>
2018-06-29 18:02:26 +0100

    Actually reuse common code

--- lambda/src/main/java/mealfu/jersey/JerseyConfig.java
+++ lambda/src/main/java/mealfu/jersey/JerseyConfig.java
@@ -8,6 +8,6 @@ public enum JerseyConfig {
     public static final ResourceConfig COMMON_JERSY_APPLICATION = new ResourceConfig()
             .packages("mealfu.servlets");
 
-    public static final ResourceConfig PROD_JERSEY_APPLICATION = new ResourceConfig()
+    public static final ResourceConfig PROD_JERSEY_APPLICATION = new ResourceConfig(COMMON_JERSY_APPLICATION)
             .register(CORSResponseFilter.just("https://crogers.github.io"));
 }
