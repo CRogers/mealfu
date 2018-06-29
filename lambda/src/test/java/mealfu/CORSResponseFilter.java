8981182e971e941485d5a25ea25bf799fe2da335
Callum Rogers <callumrogers@hotmail.co.uk>
2018-06-29 17:38:50 +0100

    Remove needless exception

--- lambda/src/test/java/mealfu/CORSResponseFilter.java
+++ lambda/src/test/java/mealfu/CORSResponseFilter.java
@@ -4,12 +4,10 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.MultivaluedMap;
-import java.io.IOException;
 
 public class CORSResponseFilter implements ContainerResponseFilter {
 
-    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
-            throws IOException {
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
 
         MultivaluedMap<String, Object> headers = responseContext.getHeaders();
 
