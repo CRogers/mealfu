129f04dff9d9cfad3535cbddea6174907bfbd884
Callum Rogers <callumrogers@hotmail.co.uk>
2018-07-01 13:37:42 +0100

    Remove pointless logging

--- lambda/src/main/java/mealfu/Handler.java
+++ lambda/src/main/java/mealfu/Handler.java
@@ -7,25 +7,18 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import mealfu.jersey.JerseyConfig;
 import org.apache.commons.io.IOUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public class Handler implements RequestStreamHandler {
-
-    private static final Logger log = LoggerFactory.getLogger(Handler.class);
-
     private static final JerseyLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler
             = JerseyLambdaContainerHandler.getAwsProxyHandler(JerseyConfig.PROD_JERSEY_APPLICATION);
 
 
     @Override
     public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
-        log.info("received request");
-
         handler.proxyStream(input, output, context);
 
         IOUtils.closeQuietly(output);
