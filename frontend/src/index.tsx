5d4ac8b33702b33bd569dfbbd5e79538ae5fc91f
Callum Rogers <callumrogers@hotmail.co.uk>
2018-07-15 12:25:42 +0100

    Get the whole react/redux shebang initialised

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,41 @@
 import * as ReactDom from 'react-dom';
 import * as React from 'react';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from "react-redux";
+import { ConnectedRouter, connectRouter, routerMiddleware } from 'connected-react-router'
+import createBrowserHistory from "history/createBrowserHistory";
+import { Route, Switch } from "react-router";
+import { Frontpage } from "./frontpage/frontpage";
 
 declare const URL_BASE: string;
+
+const reducer = (state:  string) => state;
+
+const history = createBrowserHistory();
+
+const store = createStore(
+    connectRouter(history)(reducer),
+    'hi',
+    applyMiddleware(
+        routerMiddleware(history)
+    )
+);
 
 const appContainer = document.createElement('div');
 document.body.appendChild(appContainer)
 
-ReactDom.render(<h1>Hello, world!</h1>, appContainer);
+ReactDom.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <Switch>
+                <Route exact path="/" render={() => <Frontpage />}/>
+                <Route render={() => (<div>where?</div>)} />
+            </Switch>
+        </ConnectedRouter>
+    </Provider>,
+    appContainer);
 
-const time = fetch(URL_BASE + '/test', {
+fetch(URL_BASE + '/test', {
     method: 'GET',
 })
     .then(response => {
