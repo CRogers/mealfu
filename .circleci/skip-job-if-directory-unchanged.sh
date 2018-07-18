9a489919049e85a425c8ad2cc09881a9565d851c
Callum Rogers <callumrogers@hotmail.co.uk>
2018-07-18 16:28:55 +0100

    Skip backend jobs if nothing happened

--- .circleci/skip-job-if-directory-unchanged.sh
+++ .circleci/skip-job-if-directory-unchanged.sh
@@ -2,11 +2,11 @@
 
 set -eu
 
-PATH_TO_CHECK="$1"
+PATHS_TO_CHECK="$@"
 
-DIFF="$(git diff $(echo "$CIRCLE_COMPARE_URL" | sed -n -r -e 's|^.*/(\w+)\.\.\.(\w+)$|\1 \2|p') "$PATH_TO_CHECK")"
+DIFF="$(git diff $(echo "${CIRCLE_COMPARE_URL}" | sed -n -r -e 's|^.*/(\w+)\.\.\.(\w+)$|\1 \2|p') "${PATHS_TO_CHECK}")"
 
-if [ -z "$DIFF" ]
+if [ -z "${DIFF}" ]
 then
     circleci step halt
 fi
