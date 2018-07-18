354d21b198ee42326fb7e215bd4d8103f568cda6
Callum Rogers <callumrogers@hotmail.co.uk>
2018-07-18 16:33:16 +0100

    Bash is difficult

--- .circleci/skip-job-if-directory-unchanged.sh
+++ .circleci/skip-job-if-directory-unchanged.sh
@@ -4,7 +4,7 @@ set -eu
 
 PATHS_TO_CHECK="$@"
 
-DIFF="$(git diff $(echo "${CIRCLE_COMPARE_URL}" | sed -n -r -e 's|^.*/(\w+)\.\.\.(\w+)$|\1 \2|p') "${PATHS_TO_CHECK}")"
+DIFF="$(git diff $(echo "${CIRCLE_COMPARE_URL}" | sed -n -r -e 's|^.*/(\w+)\.\.\.(\w+)$|\1 \2|p') ${PATHS_TO_CHECK})"
 
 if [ -z "${DIFF}" ]
 then
