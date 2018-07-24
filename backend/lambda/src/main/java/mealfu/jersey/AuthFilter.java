0e6cb3014b1aa7f6466c9f6cc218c0d33e5a4269
Callum Rogers <callumrogers@hotmail.co.uk>
2018-07-24 14:51:00 +0100

    Logging

--- backend/lambda/src/main/java/mealfu/jersey/AuthFilter.java
+++ backend/lambda/src/main/java/mealfu/jersey/AuthFilter.java
@@ -3,6 +3,8 @@ package mealfu.jersey;
 import mealfu.auth.AuthHeader;
 import mealfu.auth.AuthHeaderVerificationException;
 import mealfu.auth.UserAuthorizer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.container.ContainerRequestContext;
@@ -10,6 +12,8 @@ import javax.ws.rs.container.ContainerRequestFilter;
 import java.util.Optional;
 
 public class AuthFilter implements ContainerRequestFilter {
+    private static final Logger log = LoggerFactory.getLogger(AuthFilter.class);
+
     private static final String AUTHORIZATION = "Authorization";
 
     private final UserAuthorizer userAuthorizer;
@@ -25,11 +29,15 @@ public class AuthFilter implements ContainerRequestFilter {
         }
 
         String authHeaderString = Optional.ofNullable(requestContext.getHeaderString(AUTHORIZATION))
-                .orElseThrow(() -> new NotAuthorizedException(AUTHORIZATION + " header param is required"));
+                .orElseThrow(() -> {
+                    log.error("No header param. Available were " + requestContext.getHeaders().keySet());
+                    return new NotAuthorizedException(AUTHORIZATION + " header param is required");
+                });
 
         try {
             userAuthorizer.verifyUser(AuthHeader.fromString(authHeaderString));
         } catch (AuthHeaderVerificationException e) {
+            log.error("Error verifying", e);
             throw new NotAuthorizedException(e);
         }
     }
